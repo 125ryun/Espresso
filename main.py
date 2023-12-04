a5817f98b1a5de98656a7cf7e5fb2885a7528c14
125ryun <125ryun@gmail.com>
2023-12-04 16:43:44 +0900

    백업

--- main.py
+++ main.py
@@ -1,51 +1,67 @@
 import os
 import csv
-
-DATA_DIR_PATH = "./data"
-OUT_DIR_PATH = "./out"
+import pandas as pd
 
 DATA_FILE_PATH = "./data/dw_user_events.csv"
-USEFUL_COLNAMES = ["auth_id", "user_pseudo_id", "event_name", "event_timestamp_w_timezone", "screen_class", "previous_screen_class", "use_time_msec"]
-    
+TMP_FILE_PATH = "./out/tmp.csv"
+
+ID = "auth_id"
+EVENTNAME = "event_name"
+PREV_SC = "previous_screen_class"
+CURR_SC = "screen_class"
+TIMESTAMP = "event_timestamp_w_timezone"
+USETIME = "use_time_msec"
+
+USECOLS = [ID, EVENTNAME, PREV_SC, CURR_SC, TIMESTAMP, USETIME]
+
+# read csv file
 try:
-    f_r = open(DATA_FILE_PATH, "r", encoding="utf8")    
+    data = pd.read_csv(DATA_FILE_PATH, usecols=USECOLS)
 except:
-    print(f"**** ERROR: cannot open file {DATA_FILE_PATH}")
+    print(f"*** ERROR: cannot open file {DATA_FILE_PATH}")
     exit(1)
-rdr = csv.reader(f_r)
-
-if not os.path.exists(OUT_DIR_PATH):
-    os.mkdir(OUT_DIR_PATH)
-try:
-    f_tmp = open(OUT_DIR_PATH + "tmp.csv", "w", encoding="utf8")
-except:
-    print(f"**** ERROR: cannot open file {OUT_DIR_PATH}/tmp.csv")
-    exit(1)    
-wtr = csv.writer(f_tmp)
-
-colnames = []
-for i, row in enumerate(rdr):
-    if i==0: # header row
-        for j, colname in enumerate(row):
-            colnames.append(colname)
-
-f_r.close()
-f_tmp.close()
-
-# for filename in os.listdir(DATA_DIR_PATH):
-#     print(filename)
-#     f = open(DATA_DIR_PATH+"/"+filename, "r", encoding="utf8")
-#     rdr = csv.reader(f)
-        
-#     for row in rdr:
-#         print(row)
-
-#         arr = []
-#         for j, colname in enumerate(row):
-#             arr.append([j, colname])
-#         print(arr)
-        
-#         f.close()
-#         break
     
-#     print("\n")
+# make dataframe
+N = data.shape[0]
+df = pd.DataFrame(data)
+df = df[USECOLS]
+df = df.sort_values(by=[ID, TIMESTAMP])
+
+# df[TIMESTAMP] = df[TIMESTAMP] / 1000
+# col_idx_formatted_timestamp = USECOLS.index(TIMESTAMP)
+# col_name_formatted_timestamp = "fomatted_event_timestamp_w_timezone"
+# col_data_formatted_timestamp = pd.to_datetime(df[TIMESTAMP], unit="ms")
+# df.insert(col_idx_formatted_timestamp, col_name_formatted_timestamp, col_data_formatted_timestamp)
+
+# write tmp csv file
+df.to_csv(TMP_FILE_PATH, sep=",", na_rep=".", header=True,index=False, encoding="utf8")
+# exit(1)
+
+# class Node:
+#     def __init__(self):
+#         pass
+    
+#     def __init__(self, row):
+#         self.id = row[]
+    
+#     def pass(self, new):
+#         self.id = new.id
+    
+prev_ts = 0
+prev_sc = ""
+curr_ts = 0
+curr_sc = ""
+for i, row in enumerate(df.loc):
+    if prev_ts == 0:
+        prev_ts = row["event_timestamp_w_timezone"]
+        prev_sc = row["screen_class"]
+        continue
+    curr_ts = row["event_timestamp_w_timezone"]
+    curr_sc = row["screen_class"]
+    if curr_ts == prev_ts:
+        print("%%%% same timestamp at", i)
+        print(prev_sc, curr_sc)
+        input("y/n")
+    prev_ts = curr_ts
+    prev_sc = curr_sc
+    
