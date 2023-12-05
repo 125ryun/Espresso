4e979e03817e736857672b1368420e5ec548eece
125ryun <125ryun@gmail.com>
2023-12-05 16:52:22 +0900

    analyzing eventname

---
+++ eventname.py
@@ -0,0 +1,42 @@
+from define import *
+import sys
+
+class event_class_stat:
+  def __init__(self):
+    self.prev = {}
+    self.curr = {}
+  def add(self, option, sc):
+    if option == 'p':
+      self.prev[sc] = self.prev.get(sc, 0) + 1
+    if option == 'c':
+      self.curr[sc] = self.curr.get(sc, 0) + 1
+
+def get_info_abt_eventname(df):
+  eventnames = df[EVENTNAME].unique()
+
+  stats = {}
+  for i in range(df.shape[0]):
+    eventname = df.iloc[i][EVENTNAME]
+    prev_sc = df.iloc[i][PREV_SC]
+    curr_sc = df.iloc[i][CURR_SC]
+    
+    stat = stats.get(eventname, event_class_stat())
+    stat.add('p', prev_sc)
+    stat.add('c', curr_sc)
+    stats[eventname] = stat
+
+  sys.stdout = open("watch_eventnames.txt", "w")
+
+  print(eventnames)
+  for key, value in stats.items():
+    print("------------------------")
+    print(key)
+    print("--- prev ---------------")
+    for k, v in value.prev.items():
+      print(f"{k} : {v}")
+    print("--- curr ---------------")
+    for k, v in value.curr.items():
+      print(f"{k} : {v}")
+    print("------------------------\n")
+
+  sys.stdout.close()
--- main.py
+++ main.py
@@ -1,40 +1,18 @@
 from define import *
 import os
 import pandas as pd
+
 import csvdata as csvdt
-import preprocess as pre
+import eventname as en
+import preprocess as prep
 
 data = csvdt.read_data(DATA_FILE_PATH)
 df = pd.DataFrame(data)
 
-pre.preprocess(df)
+en.get_info_abt_eventname(df)
 
 exit(1)
 
+prep.format(df)
 
-prev = 0
-curr = 0
-delete_row_idx = []
-for i in range(df.shape[0]):
-    if i==0:
-        continue
-    prev = df.iloc[i-1]
-    curr = df.iloc[i]
-    if prev[ID] == curr[ID]:
-        if prev[EVENTNAME] == curr[EVENTNAME]:
-            if prev[CURR_SC] == curr[CURR_SC]:
-                if  prev[PREV_SC] == curr[PREV_SC]:
-                    delete_row_idx.append(i-1)
-                    #print("** delete index: ", i-1)
-                    if prev[CURR_SC] == curr[PREV_SC]:
-                        delete_row_idx.append(i)
-                        #print("** delete index: ", i)
-            if prev[TIMESTAMP] == curr[TIMESTAMP]:
-                delete_row_idx.append(i)
-                    #print("** same timestamp index: ", i)
-                    #input("y/n")
-    i += 1
-
-
-df.drop(delete_row_idx, axis=0, inplace=True)
-df.reset_index(drop=True, inplace=True)
+# prep.preprocess()
