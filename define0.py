a1650a7b35f6714d61ff2fe4dfc5fa47a248f8da
125ryun <125ryun@gmail.com>
2023-12-06 12:44:18 +0900

    rename define file

---
+++ define0.py
@@ -0,0 +1,14 @@
+ROW = 0
+COL = 1
+
+# data file path
+DATA_FILE_PATH = "./data/dw_user_events.csv"
+
+# col names of DATA_FILE_PATH
+ID = "auth_id"
+EVENTNAME = "event_name"
+PREV_SC = "previous_screen_class"
+CURR_SC = "screen_class"
+USETIME = "use_time_msec"
+TIMESTAMP = "event_timestamp_w_timezone"
+FORMATTED_TIMESTAMP = "fomatted_event_timestamp_w_timezone"
