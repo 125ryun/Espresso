d939993441abde6f75578caebf43ce4710b0c87d
125ryun <125ryun@gmail.com>
2023-12-21 16:53:45 +0900

    시각화 1차 완료

--- draw.py
+++ draw.py
@@ -9,50 +9,95 @@ G = nx.DiGraph()
 data_nodes = pd.read_csv("./out/stats/count_screenclass_type.csv", keep_default_na=False, na_values="-", index_col=0)
 df_nodes = pd.DataFrame(data_nodes)
 
+labels = df_nodes.index.tolist()
+node_cnt = len(labels)
+
+for i in range(node_cnt):
+    visit = df_nodes.iloc[i].sum()
+    if visit == 0: continue
+    
+    G.add_node(labels[i], visit=visit, edge_count=0)
+'''
 visits = []
 for row in df_nodes.iloc:
     visit = row.sum()
     visits.append(visit)
 df_nodes["visits"] = visits
 
-labels = df_nodes.index.tolist()
-node_sizes = [3 + visit/10 for visit in visits]
+node_sizes = [5 + visit/3 for visit in visits]
 
-for i in range(len(labels)):
-    if visit > 0:
+for i in range(node_cnt):
+    if visits[i] > 0:
         G.add_node(labels[i], size=visits[i], edge_count=0)
-
+'''
 data_edges = pd.read_csv("./out/stats/count_screenclass_series_2.csv", keep_default_na=False, na_values="-", index_col=0)
 df_edges = pd.DataFrame(data_edges)
 
+paths = df_edges.index.tolist()
+edge_cnt = len(paths)
+
+for i in range(edge_cnt):
+    weigh = df_edges.iloc[i].sum()
+    if weigh < 10: continue
+    
+    node_A, node_B = paths[i].split("-")
+    G.nodes[node_A]["edge_count"] += 1
+    G.nodes[node_B]["edge_count"] += 1
+    
+    G.add_edge(node_A, node_B, weigh=weigh, width=weigh)
+
+
+remove_nodes = []
+for node, data in G.nodes(data=True):
+    if data["visit"] < 10:
+        remove_nodes.append(node)
+G.remove_nodes_from(remove_nodes)
+
+'''
 nodes = df_edges.index.tolist()
 weighs = []
+
 for i, row in enumerate(df_edges.iloc):
     weigh = row.sum()
     weighs.append(weigh)
+    
     node = nodes[i].split("-")
     G.nodes[node[0]]['edge_count'] += 1
     G.nodes[node[1]]['edge_count'] += 1
 df_edges["weighs"] = weighs
-print(df_edges)
+
 df_edges.drop(df_edges[df_edges["weighs"]<10].index, axis=0, inplace=True)
+
 weighs = df_edges["weighs"].tolist()
-
 u_v = [(n1_n2.split("-")[0],n1_n2.split("-")[1]) for n1_n2 in df_edges.index.tolist()]
+edge_cnt = len(u_v)
 
-for i in range(len(u_v)):
+for i in range(edge_cnt):
     G.add_edge(u_v[i][0], u_v[i][1], weight = weighs[i])
+'''
 
 # 노드 및 간선 속성 설정
-node_sizes = [data['size'] * 5 + 10 for node, data in G.nodes(data=True) if data['edge_count'] > 10]
-edge_widths = [data['weight']/25+1 for u, v, data in G.edges(data=True)]
-edge_alphas = [data['weight'] for u, v, data in G.edges(data=True)]
+'''
+node_sizes = [data["size"] + 10 for node, data in G.nodes(data=True)]
+edge_widths = [data["weight"]/15+1 for u, v, data in G.edges(data=True)]
+# edge_alphas = [data['weight'] for u, v, data in G.edges(data=True)]
+'''
+
+node_colors = [data["visit"]/10 + 10 for label, data in G.nodes(data=True)]
+node_sizes = [data["visit"]*15 + 5 for label, data in G.nodes(data=True)]
+edge_colors = [data["width"]/10 + 10 for u, v, data in G.edges(data=True)]
+edge_widths = [data["width"]/15 + 1 for u, v, data in G.edges(data=True)]
 
 # 그래프 그리기
 plt.figure(figsize=(18,13))
-pos = nx.fruchterman_reingold_layout(G, k=1.5)  # 레이아웃 설정
-nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color='gray', alpha=0.5, nodelist=[node for node, data in G.nodes(data=True) if data['size'] > 0 and data['edge_count'] > 50],)
-nx.draw_networkx_edges(G, pos, width=edge_widths, edge_color='blue', alpha=0.5)
+# pos = nx.kamada_kawai_layout(G, k=1.5)  # 레이아웃 설정
+pos = nx.kamada_kawai_layout(G)  # 레이아웃 설정
+# pos = nx.spectral_layout(G)  # 레이아웃 설정
+# nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color='gray', alpha=0.5, nodelist=[label for label, data in G.nodes(data=True) if data["visit"] > 0])
+nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color=node_colors, alpha=0.7, cmap=plt.cm.Blues)
+# nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color='gray', alpha=0.5)
+nx.draw_networkx_edges(G, pos, width=edge_widths, edge_color=edge_colors, alpha=0.7, edge_cmap=plt.cm.Greys)
+# nx.draw_networkx_edges(G, pos, width=edge_widths, edge_color='blue', alpha=0.5)
 nx.draw_networkx_labels(G, pos)
 
 # 그래프 저장
